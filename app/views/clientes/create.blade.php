2cfc0ea574de17c06e286b4eda5e9e4c0ffcad40
Luciano <tonetlds@gmail.com>
2015-03-11 19:58:09 -0300

    Corrigido erro

--- app/views/clientes/create.blade.php
+++ app/views/clientes/create.blade.php
@@ -1,13 +1,5 @@
 @extends('layouts.master')
 
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> 629f3336b520499722b2d65a83ad3b56608ac9d8
->>>>>>> 1f02b56a2c906a3f38c8bc0d9b0a7c6fa5b38cce
 @section('content')
 
     <div class="container">
@@ -16,22 +8,4 @@
         
     </div>
 
-<<<<<<< HEAD
 @stop       
-=======
-@stop       
-
-<<<<<<< HEAD
-@section('scripts')
-
-	<script> 
-	    $(document).ready(function() {			
-		});
-	</script> 
-
-=======
-@section('dock')
-    
->>>>>>> 629f3336b520499722b2d65a83ad3b56608ac9d8
-@stop
->>>>>>> 1f02b56a2c906a3f38c8bc0d9b0a7c6fa5b38cce
